8fc08a9a0612c60359e056d15a86744b5a63ffb3
yuinore <yuinore@hotmail.co.jp>
2021-09-22 00:02:11 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,21 @@ Rails.application.routes.draw do
   resources :root, only: [:index]
   resources :town, only: [:index, :create]
 
-  get    "/login",   to: "sessions#new"
-  post   "/login",   to: "sessions#create"
-  delete "/logout",  to: "sessions#destroy"
+  #get    "/login",   to: "sessions#new"
+  #post   "/login",   to: "sessions#create"
+  #delete "/logout",  to: "sessions#destroy"
 
   root to: "root#index"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
 end
